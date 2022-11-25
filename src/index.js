b80a99ecf0378f931a8a5c088446db62596bea5f
YsisC <ysisclongart@gmail.com>
2022-11-25 10:56:44 -0300

    Adding the template

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
 console.log(validator);
+
+const numberName = document.getElementsByName("numeroTarjeta");
+console.log(numberName);
